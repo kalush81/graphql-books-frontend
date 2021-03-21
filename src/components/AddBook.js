e7699ef1fad884fb4d23b74bdecca755eabcb60b
kalush81 <krzysiek.kaluza@gmail.com>
2021-03-21 19:15:07 +0100

    Add mutation to add book, refetch books, little refactors

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { useQuery, gql } from "@apollo/client";
+import React, { useRef } from "react";
+import { useQuery, gql, useMutation } from "@apollo/client";
+import {allBooks} from "./BookList"
 
-const allAuthors = gql`
+const getAuthorsQuery = gql`
   {
     allAuthors {
       id
@@ -10,32 +11,69 @@ const allAuthors = gql`
   }
 `;
 
-export default function AddBook() {
-  const query = useQuery(allAuthors);
+const addBookMutation = gql`
+  mutation AddBook($title: String!, $genre: String!, $authorId: ID!) {
+    addBook(title: $title, genre: $genre, authorId: $authorId) {
+      title
+      genre
+    }
+  }
+`;
 
-  console.log("query", query);
-  if (query.loading) {
-      return "...loading"
+export default function AddBook() {
+  const [ AddBook, { loading, error } ] = useMutation(addBookMutation);
+  const query = useQuery(getAuthorsQuery);
+
+  const titleRef = useRef();
+  const genreRef = useRef();
+  const authorIdRef = useRef(null);
+
+  const addBookToDB = () => {
+    const title = titleRef.current.value;
+    const genre = genreRef.current.value;
+    let authorId = authorIdRef.current.value;
+
+    // Add book with Apollo query
+    AddBook({ variables: { title, genre, authorId }, refetchQueries: [{query: allBooks }] });
+
+    //TODO
+    // set authorIdRef.current.value to be empty string after every render
+
+  };
+  if (error) {
+    return "... ups sometihng went wrong"
+  }
+  if (loading) {
+    return " ... uploading book";
   }
   return (
     <div>
       <form>
         Add Book
         <div>
-          <label>Book name: </label>
-          <input type="text" />
+          <label>Book title: </label>
+          <input type="text" ref={titleRef} />
         </div>
         <div>
           <label>Genre :</label>
-          <input type="text" />
+          <input type="text" ref={genreRef} />
         </div>
         <div>
           <label>Author</label>
-          <select>
-            <option>Select Author</option>
-            {query.data.allAuthors.map(author => <option key={author.id}>{author.name}</option>)}
+          <select defaultValue="">
+            <option >Select Author</option>
+            {query.loading ? (
+              <option>...loading</option>
+            ) : (
+              query.data.allAuthors.map((author) => (
+                <option key={author.id} value={author.id} ref={authorIdRef}>
+                  {author.name}
+                </option>
+              ))
+            )}
           </select>
         </div>
+        <button onClick={addBookToDB}>Add Book</button>
       </form>
     </div>
   );
